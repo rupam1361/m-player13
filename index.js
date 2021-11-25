33f66f20e1a4042e109e18a60f3341666b10df5b
Rupam <rupamswargiary13@gmail.com>
2021-11-25 10:34:38 +0530

    lol

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.static("public"));
 app.use("/", mainRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__, "client", "build")));
+  app.use(express.static(path.join(__dirname, "client", "build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
