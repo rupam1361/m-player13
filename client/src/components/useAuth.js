ec08f0a222c6088f1e8e8b244571c35889de7519
Rupam <rupamswargiary13@gmail.com>
2021-11-25 10:20:52 +0530

    lol

--- client/src/components/useAuth.js
+++ client/src/components/useAuth.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const url = "/";
+const url = "http://localhost:5000";
 
 export default function useAuth(code) {
   const [accessToken, setAccessToken] = useState("");
