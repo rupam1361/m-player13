e03041bd1094781c0b4024ba426debad113ee636
Rupam <rupamswargiary13@gmail.com>
2021-11-29 19:38:55 +0530

    lol

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=8c278abff1454cee8821faaa5f55f753&response_type=code&redirect_uri=https://young-reef-94653.herokuapp.com/callback&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-read-recently-played%20user-top-read%20playlist-modify-private%20playlist-modify-public%20ugc-image-upload`;
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=8c278abff1454cee8821faaa5f55f753&response_type=code&redirect_uri=https://young-reef-94653.herokuapp.com&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-read-recently-played%20user-top-read%20playlist-modify-private%20playlist-modify-public%20ugc-image-upload`;
 
 const Login = () => {
   return (
