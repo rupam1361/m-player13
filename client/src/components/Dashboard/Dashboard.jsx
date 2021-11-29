3353c0327c45380c4fa65f72ff6e6a744d966aaf
Rupam <rupamswargiary13@gmail.com>
2021-11-29 20:55:51 +0530

    lol

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -819,10 +819,10 @@ const Dashboard = ({ code }) => {
           sx={{
             display: { lg: "flex", md: "block", sm: "block", xs: "block" },
           }}
-          style={
+          sx={
             chooseTrack
               ? {
-                  paddingBottom: 120,
+                  paddingBottom: { lg: 120, md: 120, sm: 140, xs: 140 },
                 }
               : {
                   paddingBottom: 0,
